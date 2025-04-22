482c9d026b011387d6cd7e00036afc1cf114aeda
amk09 <hanjue03@bu.edu>
2025-04-22 18:16:51 -0400

    started boundary handling

--- src/graphics/solidboundary.cpp
+++ src/graphics/solidboundary.cpp
@@ -2,17 +2,17 @@
 #include <cmath>
 #include <algorithm>
 
-void SolidBoundary::addCube(const Eigen::Vector3f& position, const Eigen::Vector3f& size, std::vector<bool>& solidCells, std::vector<SolidObject>& objects) {
+void addCube(const Eigen::Vector3f& position, const Eigen::Vector3f& size, std::vector<bool>& solidCells, std::vector<SolidObject>& objects, int N) {
     SolidObject obj;
     obj.type = SolidObject::CUBE;
     obj.position = position;
     obj.size = size;
     
     objects.push_back(obj);
-    updateSolidCells(solidCells, objects);
+    updateSolidCells(solidCells, objects, N);
 }
 
-void SolidBoundary::addSphere(const Eigen::Vector3f& position, float radius, td::vector<bool>& solidCells) {
+void addSphere(const Eigen::Vector3f& position, float radius, std::vector<bool>& solidCells, std::vector<SolidObject>& objects, int N) {
     SolidObject obj;
     obj.type = SolidObject::SPHERE;
     obj.position = position;
@@ -20,22 +20,22 @@ void SolidBoundary::addSphere(const Eigen::Vector3f& position, float radius, td:
     objects.push_back(obj);
     
     // Update solid cells mapping
-    updateSolidCells(solidCells, objects);
+    updateSolidCells(solidCells, objects, N);
 }
 
-void SolidBoundary::clear(std::vector<bool>& solidCells, std::vector<SolidObject>& objects) {
+void clear(std::vector<bool>& solidCells, std::vector<SolidObject>& objects) {
     objects.clear();
     std::fill(solidCells.begin(), solidCells.end(), false);
 }
-
-void SolidBoundary::updateSolidCells(std::vector<bool>& solidCells, const std::vector<SolidObject>& objects, int size) {
-    std::fill(m_solidCells.begin(), m_solidCells.end(), false);
+//below is still under development
+void updateSolidCells(std::vector<bool>& solidCells, const std::vector<SolidObject>& objects, int size) {
+    std::fill(solidCells.begin(), solidCells.end(), false);
     for(int k =0; k<size; k++){
         for(int j =0; j<size; j++){
             for(int i =0; i<size; i++){
-                int idx = index(i, j, k);
+                // int idx = index(i, j, k);
                 if(isInside(i, j, k)){
-                    m_solidCells[idx] = true;
+                    solidCells[idx] = true;
                 }
             }
         }
